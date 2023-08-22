e16c7bb1043d532ff22cb5ad19e98bb540f069bb
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-08-22 15:50:25 +0530

    using Router from Express JS

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+// importing my modules
+const admin=require('./routes/admin');
+const shop=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin',admin);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+// app.post('/product', (req, res, next) => {
+//     console.log(req.body);
+//     res.redirect('/');
+// });
 
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(shop);
+
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
